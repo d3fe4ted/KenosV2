458392c1d7a191fe26d0b5494d3ebee3d9aa0247
D3fe4ted <73515265+d3fe4ted@users.noreply.github.com>
2022-06-15 22:00:46 -0700

    Add files via upload

---
+++ installation.sh
@@ -0,0 +1,23 @@
+clear
+sleep 2
+echo "Installing required files for Kenos 10% [=>-------]"
+sudo apt install nodejs
+clear
+echo "Installing required files for Kenos 20% [==>-----]"
+sudo apt install perl
+clear
+echo "Installing required files for Kenos 30% [===>-----]"
+sudo apt install cat
+clear
+echo "Installing required files for Kenos 50% [=====>----]"
+sudo apt install curl
+clear
+echo "Installing required files for Kenos 70% [=======>--]"
+sudo apt install wget python
+clear
+echo "Installing required files for Kenos 100% [=========>]"
+sleep 2
+clear
+echo "Cleaning up"
+sleep 2
+rm -rf installation.sh
